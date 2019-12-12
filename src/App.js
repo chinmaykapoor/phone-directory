abd8abb8d268e8af8d33aa43c10a9dd75962a18d
Chinmay <chinmaykapoor27@gmail.com>
2019-12-12 16:58:09 +0530

    JSX: Displayed 'Phone Directory'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Phone Directory
+      </div>
+    );
+  }
 }
-
 export default App;
